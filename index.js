f3ff9ced8bb45e2ee51d978d3d8f3d2f9f9a409d
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-11-07 12:06:02 +0000

    update Code

---
+++ index.js
@@ -0,0 +1,25 @@
+// Import
+
+const app = require('express')();
+const PORT = 8080;
+
+// Listener für express-instanz
+app.listen(
+    PORT,
+    () => console.log(`server ready at http://localhost"${PORT}`)
+);
+
+/* Variante mit Code 200 */
+// app.get('/', (req, res) => {
+//     res.send(200);
+//   });
+
+/* Variante mit JSON */
+app.get("/", (req,res) =>
+{
+    res.status(200).send({
+        item: "T-shirt",
+        size: "L",
+        onstock: 20
+    })
+});
